c117b7dd0a2496fe9b3a9085f9dad09222e7a5ac
Nico <nico@vitaminlondon.com>
2015-10-21 14:04:13 +0100

    new controls

--- app.js
+++ app.js
@@ -1,163 +1,17 @@
-/*
-   config.mongoLab = {
-      address : 'ds033163.mongolab.com:33163/blox',
-      name : 'nicolasmartinos',
-      pass : '*******'
-   };
-   var mongoose = require('./node_modules/mongoose');
-
-   mongoose.connect('mongodb://' + config.mongoLab.name + ':' + config.mongoLab.pass + '@' + config.mongoLab.address, {
-      server : {
-         socketOptions : {
-            keepAlive: 1
-         }
-      },
-      replset : {
-         socketOptions : {
-            keepAlive: 1
-         }
-      }
-   });
-   var db = mongoose.connection;
-   db.on('error', console.error.bind(console, 'connection error:'));
-   var shopSchema = mongoose.Schema({
-      name: {
-         type: String,
-         required: true,
-         unique: true
-      },
-      token: {
-         type: String,
-         required: true
-      },
-      temp: {
-         type: String,
-         required: true
-      }
-   });
-   shopSchema.methods.report = function() 
-   {
-      console.log('Shop Model Report : _id = ' + this._id);
-      console.log('Shop Model Report : name = ' + this.name);
-   };
-   var blop = mongoose.model('Shop', shopSchema);
-
-   var newShop = new Shop(data);
-
-   var save = (function (error, shop)
-   {
-      if (error)
-      {
-         console.log('failed to save to db (see below)');
-         console.log(error);
-         cb(false);
-      }
-      else
-      {
-         console.log('saved : ');
-         console.log(shop);
-         cb(true, shop);
-      }
-   });
-
-
-   var find = function (name, condition, cb)
-   {
-      try
-      {
-         Shop.find(condition, function(error, shops)
-         {
-            if (error)
-            {
-               console.log('error looking for shop in DB (see below)');
-               console.log(error);
-               cb();
-            }
-            else if (shops.length)
-            {
-               console.log(shops.length + ' shop(s) found in db with the name ' + name);
-               cb(shops);
-            }
-            else
-            {
-               console.log('0 shop(s) found in db with the name ' + name);
-               cb();
-            }
-         });
-      }
-      catch (error)
-      {
-         console.log('ERROR CAUGHT looking for shop in DB (see below)');
-         console.log(error);
-      }
-   };
-
-   var remove = function (name, condition, cb)
-   {
-      try
-      {
-         self.db.find(name, condition, function (shops)
-         {
-            if (shops)
-             {
-                 shops.remove(function (err, removed)
-                 {
-                  if (err)
-                  {
-                     console.log('error removing ' + name + ' from db (see below)');
-                     console.log(err);
-                     cb(false);
-                  }
-                  else
-                  {
-                       console.log('all ' + name + ' removed from db (see below)');
-                       console.log(removed);
-                       cb(true);
-                   }
-                 });
-             }
-             else
-             {
-               console.log(name + ' not found in db');
-               cb(false);
-             }
-         });
-      }
-      catch (err)
-      {
-         console.log('ERROR CAUGHT trying to remove ' + name + ' from db (see below)');
-         console.log(err);
-         cb(false);
-      }
-   };
-*/
 // santass.herokuapp.com
 // heroku ps:scale web=1 --app santass
 
 var crypto = require('crypto');
 var app = require('express')();
-// app.use(function (req, res, next) 
-// {
-//    // Website you wish to allow to connect
-//    res.setHeader('Access-Control-Allow-Origin', 'http://christmas.vitaminlondon.com');
-// });
-
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-//io.set('origins', '*:*');
 
 server.listen(process.env.PORT || 3000);
-
-
-
-
-
 
 // Sockets object to save game code -> socked associations
 var socketCodes = {};
 
 // When a client connects...
-//io.sockets.on('connection', function(socket) 
 io.on('connection', function(socket) 
 {
    // Confirm the connection
@@ -182,10 +36,6 @@ io.on('connection', function(socket)
          // Store game code -> socket association
          socketCodes[gameCode] = socket;
          socket.gameCode = gameCode
-         console.log(" ======= socket.id ======= ");
-         console.log(socket.id);
-         console.log(" ======= io.sockets ======= ");
-         console.log(io.sockets);
          
          // Tell game client to initialize 
          //  and show the game code to the user
@@ -216,25 +66,53 @@ io.on('connection', function(socket)
       }
    });
    
-   // send jump command to game client
-   socket.on("jump", function(data)
+   socket.on("up", function(data)
    {
-      console.log("jump");
       if(socket.gameCode && socket.gameCode in socketCodes)
       {
-         socketCodes[socket.gameCode].emit("jump");
-         console.log(socket.gameCode + " >> jump");
+         socketCodes[socket.gameCode].emit("up");
       }
    });
-   
-   // send shoot command to game client
-   socket.on("shoot", function(data)
+   socket.on("down", function(data)
    {
-      console.log("shoot");
       if(socket.gameCode && socket.gameCode in socketCodes)
       {
-         socketCodes[socket.gameCode].emit("shoot");
-         console.log(socket.gameCode + " >> shoot");
+         socketCodes[socket.gameCode].emit("down");
+      }
+   });
+   socket.on("left", function(data)
+   {
+      if(socket.gameCode && socket.gameCode in socketCodes)
+      {
+         socketCodes[socket.gameCode].emit("left");
+      }
+   });
+   socket.on("right", function(data)
+   {
+      if(socket.gameCode && socket.gameCode in socketCodes)
+      {
+         socketCodes[socket.gameCode].emit("right");
+      }
+   });
+   socket.on("start", function(data)
+   {
+      if(socket.gameCode && socket.gameCode in socketCodes)
+      {
+         socketCodes[socket.gameCode].emit("start");
+      }
+   });
+   socket.on("a", function(data)
+   {
+      if(socket.gameCode && socket.gameCode in socketCodes)
+      {
+         socketCodes[socket.gameCode].emit("a");
+      }
+   });
+   socket.on("b", function(data)
+   {
+      if(socket.gameCode && socket.gameCode in socketCodes)
+      {
+         socketCodes[socket.gameCode].emit("b");
       }
    });
 });
